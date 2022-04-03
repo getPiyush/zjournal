d4a088170707bd8d535bcd2d1eec426ce96d35e4
Piyush Praharaj <piyush.praharaj@outlookcom>
2022-04-03 09:25:35 +0530

    dev marker

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-console.log("NODE_ENV =",process.env.NODE_ENV);
+
 const rootElement = document.getElementById("root");
+
 render(
   <BrowserRouter>
     <App />
--- src/web/Footer.tsx
+++ src/web/Footer.tsx
@@ -5,6 +5,14 @@ export default function Footer() {
         <span className="text-muted small-text">
           &copy; Copyright 2022, Pharmaceutical Updates by Chandrasekhar Panda.
         </span>
+        {process.env.NODE_ENV !== "production" && (
+        <span
+          title="Note : You are currently seeing development version, you might come across some alpha experiences."
+          style={{ color: "crimson" }}
+        >
+          {' {'}<i className="bi bi-bug-fill"></i>{'} '}
+        </span>
+      )}
       </div>
     </footer>
   );
