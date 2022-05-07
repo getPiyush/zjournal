ad76cfd02be0f59af45d4cee3f943dfbde97ca32
Piyush Praharaj <piyush.praharaj@outlookcom>
2022-05-08 00:26:07 +0530

    logo

--- src/web/components/Logo.tsx
+++ src/web/components/Logo.tsx
@@ -1,9 +1,8 @@
 export const Logo = () => {
-    return (
-    <img
-      style={{ padding: "0px" }}
-      src="/images/patrikaz_logo_eng.jpeg"
-      height="40px"
-    />
+  return (
+    <span className="blog-header blog-header-logo">
+      <b>Pharmaceutical Updates</b>
+      <div className="logo-subtext"><i>by Chandrasekhar Panda</i></div>
+    </span>
   );
 };
