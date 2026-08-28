652cb6b755cd69afa483129909de514fad83bacf
Piyush Praharaj <piyush.praharaj@outlook.com>
2026-08-27 20:27:21 -0400

    clopase responsive header menu on item select

--- patrikaz/src/web/Header.tsx
+++ patrikaz/src/web/Header.tsx
@@ -1,19 +1,30 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useRef } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Logo } from "../remotes/uiLibraryComponents";
 import { updatePage } from "../datastore/actions/JournalActions";
 import { useJournal } from "../datastore/contexts/JournalContext";
 import { applicationProperties } from "../ApplicationConstants";
 
+declare const bootstrap: any;
+
 export default function Header() {
   const { dispatch } = useJournal();
   const location = useLocation().pathname;
   const navigate = useNavigate();
+  const navCollapseRef = useRef<HTMLDivElement>(null);
+
+  const closeMobileNav = () => {
+    const collapseEl = navCollapseRef.current;
+    if (collapseEl && collapseEl.classList.contains("show")) {
+      bootstrap.Collapse.getOrCreateInstance(collapseEl).hide();
+    }
+  };
 
   const linkClicked = (event: React.MouseEvent<HTMLAnchorElement>) => {
     const target: any = event.target;
     const buttonFlag = target.getAttribute("button-flag");
     updatePage(buttonFlag, dispatch);
+    closeMobileNav();
   };
   return (
     <header>
@@ -40,6 +51,7 @@ export default function Header() {
           <div
             className="collapse navbar-collapse justify-content-end"
             id="navbarCollapse"
+            ref={navCollapseRef}
           >
             <ul className="navbar-nav mb-2 mb-md-0">
               <li className="nav-item">
@@ -49,6 +61,7 @@ export default function Header() {
                   }`}
                   aria-current="page"
                   to="/home"
+                  onClick={closeMobileNav}
                 >
                   Home
                 </Link>
@@ -60,6 +73,7 @@ export default function Header() {
                   }`}
                   aria-current="page"
                   to="/aboutus"
+                  onClick={closeMobileNav}
                 >
                   About
                 </Link>
